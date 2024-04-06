8bf44b9b2dcef2038f70bd8f70517a23579453a9
hwanzar <giadh1103@gmail.com>
2024-04-06 09:49:42 +0700

    add notes

--- index.js
+++ index.js
@@ -4,17 +4,25 @@ const MQTT_SERVER = "mqtt.ohstem.vn"
 const MQTT_PORT = 1883
 const MQTT_USERNAME = "Yolo:HomeZar"
 const MQTT_PASSWORD = ""
+// V1 : nhiệt độ
+// V2 : độ ẩm
+// V3 : ánh sáng
+
+// MQTT_TOPIC_PUB: dùng để PUBLISH.
 const MQTT_TOPIC_PUB = [
     `${MQTT_USERNAME}/feeds/V1`,
     `${MQTT_USERNAME}/feeds/V2`,
     `${MQTT_USERNAME}/feeds/V3`,
 ]
+
+// MQTT_TOPIC_SUB: dùng để SUBSCRIBE.
 const MQTT_TOPIC_SUB = [
     `${MQTT_USERNAME}/feeds/V1`,
     `${MQTT_USERNAME}/feeds/V2`,
     `${MQTT_USERNAME}/feeds/V3`,
 ]
 
+// CONNECT vào server ohstem.
 const mqttClient = connect({
     host: MQTT_SERVER,
     port: MQTT_PORT,
@@ -22,6 +30,7 @@ const mqttClient = connect({
     password: MQTT_PASSWORD,
 })
 
+// connect thành công thì sẽ subscribe các topic V1, V2 , V3
 mqttClient.on("connect", () => {
     console.log("Connected successfully!")
     MQTT_TOPIC_SUB.forEach((topic) => {
@@ -35,6 +44,16 @@ mqttClient.on("connect", () => {
     })
 })
 
+//  QUAN TRỌNG: làm sao lưu lại message mỗi khi nhận message thành 1 database.
+// let saveMessageToDB = function (message) {   ... }
+// let saveMessageToDB = async function (      message) {
+//     const data = JSON.parse(message)
+
+//     // LƯU VÀO DB, chưa nghĩ ra
+//     // ....
+// }
+
+//  QUAN TRỌNG: làm sao lưu lại message mỗi khi nhận message thành 1 database??
 mqttClient.on("message", (topic, message) => {
     console.log(`Received message ${message.toString()} on topic ${topic}`)
 })
@@ -42,6 +61,8 @@ mqttClient.on("message", (topic, message) => {
 mqttClient.on("error", (err) => {
     console.error("MQTT error:", err)
 })
+
+// DEMO THỬ 1 quá trình publish message lên server bằng mqtt
 
 let counter = 0
 
